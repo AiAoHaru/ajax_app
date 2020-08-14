70260c27c704c63fe9d3f3497c9ef382c48226cb
AiAoHaru <3110aiaoharu@gmail.com>
2020-08-14 19:21:24 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # get 'posts', to: 'posts#index'
   # get 'posts/new', to:'posts#new'  ⇨  投稿完了のページを使用しないため
   post 'posts', to: 'posts#create'
+  # get 'posts', to: 'posts#checked'  ⇨  queryパラメーターでのエンドポイントのルーティング
+  get 'posts/:id', to: 'posts#checked'  # pathパラメーターでのエンドポイントのルーティング
 end
